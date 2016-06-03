dde45143f7799f611cf19c107246a2acadf6d664
Martin Leon Preciado <Martin@macbook-air-de-martin.local>
2016-06-02 22:52:11 -0500

    All Chapters

--- Lab03/Start.h
+++ Lab03/Start.h
@@ -15,9 +15,60 @@
 @property (strong, nonatomic) IBOutlet UIButton *BttL1;
 @property (strong, nonatomic) IBOutlet UIButton *BttD1;
 @property (strong, nonatomic) IBOutlet UIImageView *Answ1;
+@property (strong, nonatomic) IBOutlet UIButton *BttL2;
+@property (strong, nonatomic) IBOutlet UIButton *BttD2;
+@property (strong, nonatomic) IBOutlet UIImageView *Answ2;
+@property (strong, nonatomic) IBOutlet UIButton *BttL3;
+@property (strong, nonatomic) IBOutlet UIButton *BttD3;
+@property (strong, nonatomic) IBOutlet UIImageView *Answ3;
+@property (strong, nonatomic) IBOutlet UIButton *BttL4;
+@property (strong, nonatomic) IBOutlet UIButton *BttD4;
+@property (strong, nonatomic) IBOutlet UIImageView *Answ4;
+@property (strong, nonatomic) IBOutlet UIButton *BttL5;
+@property (strong, nonatomic) IBOutlet UIButton *BttD5;
+@property (strong, nonatomic) IBOutlet UIImageView *Answ5;
+@property (strong, nonatomic) IBOutlet UIButton *BttL6;
+@property (strong, nonatomic) IBOutlet UIButton *BttD6;
+@property (strong, nonatomic) IBOutlet UIImageView *Answ6;
+@property (strong, nonatomic) IBOutlet UIButton *BttL7;
+@property (strong, nonatomic) IBOutlet UIButton *BttD7;
+@property (strong, nonatomic) IBOutlet UIImageView *Answ7;
+@property (strong, nonatomic) IBOutlet UIButton *BttL8;
+@property (strong, nonatomic) IBOutlet UIButton *BttD8;
+@property (strong, nonatomic) IBOutlet UIImageView *Answ8;
+@property (strong, nonatomic) IBOutlet UIButton *BttL9;
+@property (strong, nonatomic) IBOutlet UIButton *BttD9;
+@property (strong, nonatomic) IBOutlet UIImageView *Answ9;
+@property (strong, nonatomic) IBOutlet UIButton *BttL10;
+@property (strong, nonatomic) IBOutlet UIButton *BttD10;
+@property (strong, nonatomic) IBOutlet UIImageView *Answ10;
+@property (strong, nonatomic) IBOutlet UIButton *BttLNext;
+
+
+
 
 - (IBAction)BttD1Press:(id)sender;
 - (IBAction)BttL1Press:(id)sender;
+- (IBAction)BttD2Press:(id)sender;
+- (IBAction)BttL2Press:(id)sender;
+- (IBAction)BttD3Press:(id)sender;
+- (IBAction)BttL3Press:(id)sender;
+- (IBAction)BttD4Press:(id)sender;
+- (IBAction)BttL4Press:(id)sender;
+- (IBAction)BttD5Press:(id)sender;
+- (IBAction)BttL5Press:(id)sender;
+- (IBAction)BttD6Press:(id)sender;
+- (IBAction)BttL6Press:(id)sender;
+- (IBAction)BttD7Press:(id)sender;
+- (IBAction)BttL7Press:(id)sender;
+- (IBAction)BttD8Press:(id)sender;
+- (IBAction)BttL8Press:(id)sender;
+- (IBAction)BttD9Press:(id)sender;
+- (IBAction)BttL9Press:(id)sender;
+- (IBAction)BttD10Press:(id)sender;
+- (IBAction)BttL10Press:(id)sender;
+- (IBAction)BttLNext:(id)sender;
+
 
 @end
 
--- Lab03/Start.m
+++ Lab03/Start.m
@@ -21,7 +21,7 @@ char Control[10]={0};
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
     [scroller setScrollEnabled:YES];
-    [scroller setContentSize: CGSizeMake(320, 800)];
+    [scroller setContentSize: CGSizeMake(320, 1450)];
 }
 
 - (void)didReceiveMemoryWarning {
@@ -47,5 +47,162 @@ char Control[10]={0};
     }
 }
 
+- (IBAction)BttD2Press:(id)sender {
+    if(Control[1]==0)
+    {
+        Counter++;
+        Control[1]=1;
+        self.Answ2.image= [UIImage imageNamed:@"Correct.png"];
+    }
+}
+
+- (IBAction)BttL2Press:(id)sender {
+    if(Control[1]==0)
+    {
+        Control[1]=1;
+        self.Answ2.image= [UIImage imageNamed:@"Wrong.png"];
+    }
+}
+
+- (IBAction)BttD3Press:(id)sender {
+    if(Control[2]==0)
+    {
+        Control[2]=1;
+        self.Answ3.image= [UIImage imageNamed:@"Wrong.png"];
+    }
+}
+
+- (IBAction)BttL3Press:(id)sender {
+    if(Control[2]==0)
+    {
+        Counter++;
+        Control[2]=1;
+        self.Answ3.image= [UIImage imageNamed:@"Correct.png"];
+    }
+}
+
+- (IBAction)BttD4Press:(id)sender {
+    if(Control[3]==0)
+    {
+        Counter++;
+        Control[3]=1;
+        self.Answ4.image= [UIImage imageNamed:@"Correct.png"];
+    }
+}
+
+- (IBAction)BttL4Press:(id)sender {
+    if(Control[3]==0)
+    {
+        Control[3]=1;
+        self.Answ4.image= [UIImage imageNamed:@"Wrong.png"];
+    }
+}
+
+- (IBAction)BttD5Press:(id)sender {
+    if(Control[4]==0)
+    {
+        Control[4]=1;
+        self.Answ5.image= [UIImage imageNamed:@"Wrong.png"];
+    }
+}
+
+- (IBAction)BttL5Press:(id)sender {
+    if(Control[4]==0)
+    {
+        Counter++;
+        Control[4]=1;
+        self.Answ5.image= [UIImage imageNamed:@"Correct.png"];
+    }
+}
+
+- (IBAction)BttD6Press:(id)sender {
+    if(Control[5]==0)
+    {
+        Counter++;
+        Control[5]=1;
+        self.Answ6.image= [UIImage imageNamed:@"Correct.png"];
+    }
+}
+
+- (IBAction)BttL6Press:(id)sender {
+    if(Control[5]==0)
+    {
+        Control[5]=1;
+        self.Answ6.image= [UIImage imageNamed:@"Wrong.png"];
+    }
+}
+
+- (IBAction)BttD7Press:(id)sender {
+    if(Control[6]==0)
+    {
+        Counter++;
+        Control[6]=1;
+        self.Answ7.image= [UIImage imageNamed:@"Correct.png"];
+    }
+}
+
+- (IBAction)BttL7Press:(id)sender {
+    if(Control[6]==0)
+    {
+        Control[6]=1;
+        self.Answ7.image= [UIImage imageNamed:@"Wrong.png"];
+    }
+}
+
+- (IBAction)BttD8Press:(id)sender {
+    if(Control[7]==0)
+    {
+        Counter++;
+        Control[7]=1;
+        self.Answ8.image= [UIImage imageNamed:@"Correct.png"];
+    }
+}
+
+- (IBAction)BttL8Press:(id)sender {
+    if(Control[7]==0)
+    {
+        Control[7]=1;
+        self.Answ8.image= [UIImage imageNamed:@"Wrong.png"];
+    }
+}
+
+
+- (IBAction)BttD9Press:(id)sender {
+    if(Control[8]==0)
+    {
+        Control[8]=1;
+        self.Answ9.image= [UIImage imageNamed:@"Wrong.png"];
+    }
+}
+
+- (IBAction)BttL9Press:(id)sender {
+    if(Control[8]==0)
+    {
+        Counter++;
+        Control[8]=1;
+        self.Answ9.image= [UIImage imageNamed:@"Correct.png"];
+    }
+}
+
+- (IBAction)BttD10Press:(id)sender {
+    if(Control[9]==0)
+    {
+        Counter++;
+        Control[9]=1;
+        self.Answ10.image= [UIImage imageNamed:@"Correct.png"];
+    }
+}
+
+- (IBAction)BttL10Press:(id)sender {
+    if(Control[9]==0)
+    {
+        Control[9]=1;
+        self.Answ10.image= [UIImage imageNamed:@"Wrong.png"];
+    }
+}
+
+- (IBAction)BttLNext:(id)sender {
+
+}
 
 @end
